d1582a3abf7a6f068011ac9e9112a67ddb398d0b
DerekMwirigi <derekmwirigi@gmail.com>
2019-08-29 09:38:41 +0300

    Today

--- Config/app.config.js
+++ Config/app.config.js
@@ -1,30 +1,30 @@
 app = {
-    name: 'Queue Manegment System',
-    nameshort: 'QuMS',
+    name: ' Bahati Hospital',
+    nameshort: 'QMS',
     version: '1.0',
     website:'https://blog.calista.co.ke/apps/?appId=32wekj3242i454mh09345'
 },
 client = {
-    name: 'Brenzel Hospital, Kenya',
+    name: 'Bahati Hospital, Kenya',
     nameshort: 'BH',
     website: '',
     emails:[
 
     ],
     phones:[
-        '+254 706 828 557'
+        '+254 721932795'
     ]
 },
 developer = {
-    name: 'Calista',
+    name: 'Beatrice Kirunda',
     website: '',
     social:{
         linkedIn: ''
     },
     emails:[
-
+        'beatricekirunda@gmail.com'
     ],
     phones:[
-
+        '+254 721932795'
     ]
 }
